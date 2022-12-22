96bdea80496aa247f12af81f977da167664ec46d
maria <m.n.chernigov@gmail.com>
2022-12-22 15:59:41 +0700

    dz4

--- dz1/Calculator.java
+++ dz1/Calculator.java
@@ -1,5 +1,7 @@
 package dz1;
 
+import seminar4.ArrayStack;
+
 import java.io.IOException;
 import java.util.logging.*;
 
@@ -7,9 +9,33 @@ import java.util.Scanner;
 //Реализовать простой калькулятор
 public class Calculator {
     public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        System.out.print("Введите первое число: ");
+        double num1 = reader.nextDouble();
+        System.out.print("Введите оператор (+, -, *, /): ");
+        char operator = reader.next().charAt(0);
+        System.out.print("Введите второе число: ");
+        double num2 = reader.nextDouble();
+        double ans = calculator(num1, num2, operator);
 
-        Logger logger = Logger.getLogger(Calculator.class.getName());
+        ArrayStack<Double> myStack = new ArrayStack();
 
+
+        myStack.push(ans);
+        System.out.println("Если хотите отменить действие введите 'del'");
+        System.out.println("Для выхода из калькулятора введите 'exit'");
+        Scanner scanner = new Scanner(System.in);
+        String back = scanner.next();
+        if (back.equals("del")){
+            myStack.pop();
+        }
+        if (back.equals("exit")) {
+            Logger logger = Logger.getLogger(Calculator.class.getName());
+            logging(logger);
+            logger.log(Level.INFO, "Ввели: " + num1 + operator + num2 + " = " + ans);
+        }
+    }
+    private static void logging(Logger logger) {
         try {
             FileHandler fh = new FileHandler("calcLog.txt", true);
             SimpleFormatter sFormat = new SimpleFormatter();
@@ -23,18 +49,9 @@ public class Calculator {
             logger.log(Level.SEVERE,
                     "Не удалось создать файл лога из-за ошибки ввода-вывода.", e);
         }
-
-        double num1;
-        double num2;
+    }
+    private static double calculator(double num1, double num2,  char operator) {
         double ans;
-        char operator;
-        Scanner reader = new Scanner(System.in);
-        System.out.print("Введите первое число: ");
-        num1 = reader.nextDouble();
-        System.out.print("Введите оператор (+, -, *, /): ");
-        operator = reader.next().charAt(0);
-        System.out.print("Введите второе число: ");
-        num2 = reader.nextDouble();
         switch (operator) {
             case '+':
                 ans = num1 + num2;
@@ -50,12 +67,11 @@ public class Calculator {
                 break;
             default:
                 System.out.println("Ошибка! Вы ввели некорректные данные");
-                return;
+                return 0;
         }
         System.out.print("результат:\n");
         System.out.printf(num1 + " " + operator + " " + num2 + " = " + ans + '\n');
-
-        logger.log(Level.INFO,"num1 = " + num1  + ", " + "operator = " + "'" + operator + "'" + ", " + "num2 = " + num2  + ", " + "результат равен: " + ans);
-
+        return ans;
     }
+
 }
